eb193cbe7695cb55b0a9bf4d4a5adf81a626e63b
Ronal  Hernández <ronalmazariegos2015@gmail.com>
2020-10-08 18:12:14 -0600

    Updates

---
+++ precache-manifest.9137446f7a310bea749cebfec2dd8ffb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589cd3ca35c74ac4148473d03682e8be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4feadae7ffef5107def",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "42c798e355d3feed3326",
+    "url": "./static/js/2.43584be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.43584be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4feadae7ffef5107def",
+    "url": "./static/js/main.07bb8fb6.chunk.js"
+  },
+  {
+    "revision": "63d140a8cb768db0bf57",
+    "url": "./static/js/runtime-main.53a21ed8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
